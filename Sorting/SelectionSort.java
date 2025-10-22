e4886b810f337b192f7d852d950f67f1aa15f909
Prathama <pratihar.prathama@gmail.com>
2025-10-22 21:18:43 +0530

    Updated the Selection Sort Algorithm

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class SelectionSort {
     static void selectionsort(int arr[], int n){
-        int i,j,min,temp;
-        for(i=0;i<n-1;i++){
-            min=i;
-            for(j=i+1;j<n;j++){
-                if(arr[j]<arr[min]){
-                    min=j;
+        int i, j, min, temp;
+        for(i=0; i<n-1; i++){
+            min = i;
+            for(j=i+1; j<n; j++){
+                if(arr[j] < arr[min]){
+                    min = j;
             }
         }
-        temp=arr[i];
-        arr[i]=arr[min];
-        arr[min]=temp;  
+        temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;  
     }    
 }
     //Method to print the array
@@ -22,21 +22,21 @@ public class SelectionSort {
         int n = arr.length;
         int i;
         System.out.println("Sorted array through selection sort: \n");
-        for(i=0;i<n;i++){
+        for(i=0; i<n; i++){
             System.out.print(arr[i] + " ");
             }
 
     }
 
     public static void main(String args[]){
-        int n,i;
-        Scanner sc=new Scanner(System.in);
+        int n, i;
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of elements in array: ");
-        n=sc.nextInt();
+        n = sc.nextInt();
          int[] arr = new int[n];
         System.out.print("Enter the array: \n");
-        for(i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+        for(i=0; i<n; i++){
+            arr[i] = sc.nextInt();
         }
         selectionsort(arr, n);
         print(arr);
